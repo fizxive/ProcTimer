e67c24a1ec0387687cc8096e3d6c007cf2597938
fizxive <imaginary.rhombus@gmail.com>
2018-12-14 22:01:30 +0900

    ViewModel の修正。
    メンバの名称修正とプライマリコンストラクタを使用するように変更。
    実装の誤りを修正するため.

--- app/src/main/java/com/imaginaryrhombus/proctimer/ui/timer/TimerViewModel.kt
+++ app/src/main/java/com/imaginaryrhombus/proctimer/ui/timer/TimerViewModel.kt
@@ -2,8 +2,9 @@ package com.imaginaryrhombus.proctimer.ui.timer
 
 import android.arch.lifecycle.ViewModel
 
-class TimerViewModel : ViewModel() {
-
-    /// 実行中タイマー
-    var primaryTimer = TimerModel(5 * 60.0f)
+/**
+ * タイマー用の ViewModel.
+ * @param workingTimer 動作しているタイマー
+ */
+class TimerViewModel(var workingTimer : TimerModel = TimerModel(5 * 60.0f)) : ViewModel() {
 }
