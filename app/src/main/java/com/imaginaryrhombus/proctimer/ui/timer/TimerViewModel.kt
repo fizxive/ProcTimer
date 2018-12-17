3113261b79b788da64664e5bb4e3076ebe11e710
fizxive <imaginary.rhombus@gmail.com>
2018-12-18 00:00:00 +0900

    これまでのコミットに合わせて TimerViewModel を変更.
    作りかけだけどビルドとして通るところまでをコミット.

--- app/src/main/java/com/imaginaryrhombus/proctimer/ui/timer/TimerViewModel.kt
+++ app/src/main/java/com/imaginaryrhombus/proctimer/ui/timer/TimerViewModel.kt
@@ -1,14 +1,82 @@
 package com.imaginaryrhombus.proctimer.ui.timer
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
-import android.databinding.ObservableField
+import android.os.Handler
+import java.util.*
 
 /**
  * タイマー用の ViewModel.
- * @param workingTimer 動作しているタイマー
  */
-class TimerViewModel(private var workingTimer : TimerModel) : ViewModel() {
+class TimerViewModel : ViewModel() {
 
-    var timerText = ObservableField<String>(workingTimer.text)
+    /// タイマー本体.(外部からの直接設定をしないようにする)
+    var timer = MutableLiveData<TimerModel>()
+    private set
 
+    /// 時間経過の判定を行う間隔.
+    val tickInterval = 10L
+
+    /// 時間経過用のハンドラ.
+    val tickHandler = Handler()
+
+    /**
+     * 時間経過を司る内部クラス
+     * TODO : 別ファイルにする. テストしやすくするため.
+     */
+    class TimeTicker {
+
+        private val calender : Calendar = Calendar.getInstance(Locale.getDefault())
+
+        private var prevMilliseconds = 0L
+
+        var latestTick = 0.0f
+        private set
+
+        init {
+            setPrevious()
+        }
+
+        fun tick() {
+            val currentMilliseconds = calender.timeInMillis
+            val deltaMilliseconds = currentMilliseconds - prevMilliseconds
+
+            latestTick = deltaMilliseconds / 1000.0f
+            prevMilliseconds = currentMilliseconds
+
+            setPrevious()
+        }
+
+        fun setPrevious() {
+            prevMilliseconds = calender.timeInMillis
+        }
+    }
+
+    private var timeTicker = TimeTicker()
+
+    /// 時間経過用のワーカー.
+    val tickRunner = object : Runnable {
+
+        override fun run() {
+
+            timeTicker.tick()
+
+            timer.value!!.tick(timeTicker.latestTick)
+
+            tickHandler.postDelayed(this, tickInterval)
+        }
+    }
+
+    init {
+        timer.value = TimerModel()
+    }
+
+    fun startTick() {
+        timeTicker.setPrevious()
+        tickHandler.post(tickRunner)
+    }
+
+    fun stopTick() {
+        tickHandler.removeCallbacks(tickRunner)
+    }
 }
