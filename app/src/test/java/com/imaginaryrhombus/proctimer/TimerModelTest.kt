59856def19bfc6a86c7325d10c96b8ff7499e101
fizxive <imaginary.rhombus@gmail.com>
2018-12-21 02:44:03 +0900

    テストに Robolectric を使用するように変更.
    Activity が必要になるので、それを容易に扱えるように.

--- app/src/test/java/com/imaginaryrhombus/proctimer/TimerModelTest.kt
+++ app/src/test/java/com/imaginaryrhombus/proctimer/TimerModelTest.kt
@@ -1,24 +1,46 @@
 package com.imaginaryrhombus.proctimer
 
+import android.content.Context
 import com.imaginaryrhombus.proctimer.ui.timer.TimerModel
-import org.junit.Test
+import com.imaginaryrhombus.proctimer.constants.TimerConstants
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
 
+
+/// テスト試行回数.
+const val TEST_TIMES : Int = 50
+
+@RunWith(RobolectricTestRunner::class)
 class TimerModelTest {
 
-    private val testTimes = 50
+    /// テスト用の Activity.
+    private val testActivity = Robolectric.setupActivity(TimerActivity::class.java)!!
 
     /// 秒数設定のための無作為な少数を生成する.
-    fun getRandomFloat() :Float {return Math.random().toFloat() * Float.MAX_VALUE}
+    private fun getRandomFloat() : Float {return Math.random().toFloat() * Float.MAX_VALUE}
+
+    /** テスト用の TimerModel を作成する.
+     * @param seconds 設定する秒数.
+     * @see TimerModel
+     */
+    private fun createTimerModel(seconds : Float) : TimerModel {
+        val context = testActivity.applicationContext
+        context.getSharedPreferences(TimerConstants.PREFERENCE_NAME, Context.MODE_PRIVATE)
+            .edit().putFloat(TimerConstants.PREFERENCE_PARAM_SEC_NAME, seconds)
+        return TimerModel(context)
+    }
 
     /**
      * 秒設定テスト
      */
     @Test
     fun second_correct() {
-        repeat(testTimes, fun (_: Int) {
+        repeat(TEST_TIMES, fun (_: Int) {
             val testSeconds = getRandomFloat()
-            var timerModel = TimerModel(testSeconds)
+            val timerModel = createTimerModel(testSeconds)
             assertEquals(timerModel.seconds, testSeconds)
         })
     }
@@ -28,10 +50,10 @@ class TimerModelTest {
      */
     @Test
     fun tick_correct() {
-        repeat(testTimes, fun (_: Int){
+        repeat(TEST_TIMES, fun (_: Int){
             val testSeconds = getRandomFloat() / 2
             val testTickSeconds = getRandomFloat() / 2
-            var timerModel = TimerModel(testSeconds)
+            val timerModel = createTimerModel(testSeconds)
 
             timerModel.tick(testTickSeconds)
 
