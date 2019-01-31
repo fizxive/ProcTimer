1040708e91e7075dc8bacad1044d220a33259f4f
fizxive <imaginary.rhombus@gmail.com>
2019-02-01 00:38:52 +0900

    Firebase Remote Config の使用テスト
    動作確認のため、Toast を用いて取得したバージョンを表示している.
    
    後に、fetch ごとに動作バージョンの比較、強制アップデートダイアログの表示を行うようにする.

--- app/src/main/java/com/imaginaryrhombus/proctimer/TimerActivity.kt
+++ app/src/main/java/com/imaginaryrhombus/proctimer/TimerActivity.kt
@@ -2,6 +2,8 @@ package com.imaginaryrhombus.proctimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.imaginaryrhombus.proctimer.ui.timer.TimerFragment
 
 class TimerActivity : AppCompatActivity() {
@@ -9,6 +11,19 @@ class TimerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.timer_activity)
+
+        val remoteConfig = FirebaseRemoteConfig.getInstance()
+        val defaultValueMap = HashMap<String, Any>().apply {
+            put("LEAST_VERSION", "0.0.0.0")
+        }
+        remoteConfig.setDefaults(defaultValueMap)
+        remoteConfig.fetch().addOnCompleteListener {
+            remoteConfig.activateFetched()
+            Toast.makeText(this, "Remote Config fetch complete,\ncurrent least version is ${remoteConfig.getString("LEAST_VERSION")}.", Toast.LENGTH_LONG).show()
+        }.addOnFailureListener {
+            Toast.makeText(this, "Remote Config fetch failure.", Toast.LENGTH_LONG).show()
+        }
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, TimerFragment.newInstance())
