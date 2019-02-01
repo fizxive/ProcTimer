1fff396dd3cf62fb4099f5d09058e9ec50b7a85c
fizxive <imaginary.rhombus@gmail.com>
2019-02-02 00:32:39 +0900

    アプリケーションのバージョン情報を比較するための関数群の用意
    TODO テストする.

---
+++ app/src/main/java/com/imaginaryrhombus/proctimer/constants/ApplicationUtility.kt
@@ -0,0 +1,69 @@
+package com.imaginaryrhombus.proctimer.constants
+
+import android.content.Context
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.imaginaryrhombus.proctimer.BuildConfig
+
+/**
+ * アプリケーション全体に関わるユーティリティ.
+ * @todo このクラス全体のテスト.
+ */
+class ApplicationUtility {
+    companion object {
+        /**
+         * アップデートが必要か(実行中のアプリが強制アップデート対象か)を確認する.
+         * @param context アプリケーションのコンテキスト.
+         * @return 強制アップデートならば true.
+         */
+        fun checkUpdateRequired(context: Context): Boolean {
+            val firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+
+            val configName = if (BuildConfig.DEBUG) "LEAST_VERSION_DEBUG" else "LEAST_VERSION"
+
+            firebaseRemoteConfig.setDefaults(HashMap<String, Any>().apply {
+                put(configName, "0.0.0.0")
+            })
+
+            var ret = false
+
+            firebaseRemoteConfig.fetch().addOnCompleteListener {
+                firebaseRemoteConfig.activateFetched()
+
+                val requiredVersion = firebaseRemoteConfig.getString(configName)
+
+                val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+                val currentVersion = packageInfo.versionName
+
+                ret = isRequiredUpdate(currentVersion, requiredVersion)
+            }
+
+            return ret
+        }
+
+        /**
+         * バージョン文字列を比較してアップデートが必要かを判断する.
+         * @param currentVersion 現在のバージョン文字列.
+         * @param requiredVersion 強制アップデートバージョン文字列.
+         * @return アップデートが必要なバージョンの場合は true.
+         * @note x.x.x.x のようにピリオドで区切られたバージョンで動作する.ただし、数字一桁でも問題ない.
+         */
+        private fun isRequiredUpdate(currentVersion: String, requiredVersion: String): Boolean {
+
+            val currentSubVersion = currentVersion.substringBefore(".", "0")
+            val requiredSubVersion = requiredVersion.substringBefore(".", "0")
+
+            val currentSubIntVersion = currentSubVersion.toIntOrNull() ?: 0
+            val requiredSubIntVersion = requiredSubVersion.toIntOrNull() ?: 0
+
+            if (requiredSubIntVersion > currentSubIntVersion) {
+                return true
+            } else if (currentSubIntVersion > requiredSubIntVersion) {
+                return false
+            } else {
+                return isRequiredUpdate(
+                    currentVersion.substringAfter(".", ""),
+                    requiredVersion.substringAfter(".", ""))
+            }
+        }
+    }
+}
