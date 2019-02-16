bfbd78506b96f982eaca31163976e3ff1f486322
fizxive <imaginary.rhombus@gmail.com>
2019-02-17 03:04:58 +0900

    未使用 import 削除漏れ

--- app/src/test/java/com/imaginaryrhombus/proctimer/UpdateCheckerTest.kt
+++ app/src/test/java/com/imaginaryrhombus/proctimer/UpdateCheckerTest.kt
@@ -8,7 +8,6 @@ import com.imaginaryrhombus.proctimer.application.UpdateChecker
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
