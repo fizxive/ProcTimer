b2d2bb8fcb8b78859d9a7c24c3af862afc8dd308
fizxive <imaginary.rhombus@gmail.com>
2019-02-26 00:23:50 +0900

    ナビゲーションドロワー内にプライバシーポリシーを移動
    それに伴ってアプリのテーマをツールバーが設定されていないものに変更.
    必要な各種設定を追加。
    ダークモード実装の準備。

--- app/src/main/java/com/imaginaryrhombus/proctimer/TimerActivity.kt
+++ app/src/main/java/com/imaginaryrhombus/proctimer/TimerActivity.kt
@@ -3,25 +3,109 @@ package com.imaginaryrhombus.proctimer
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.imaginaryrhombus.proctimer.application.UpdateChecker
 import com.imaginaryrhombus.proctimer.ui.timer.TimerFragment
+import kotlinx.android.synthetic.main.timer_activity.*
 
 class TimerActivity : AppCompatActivity(),
     UpdateChecker.UpdateRequiredListener {
 
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Light)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.timer_activity)
 
         UpdateChecker(this).checkUpdateRequired()
 
+        setSupportActionBar(toolbar)
+        checkNotNull(supportActionBar).run {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeButtonEnabled(true)
+        }
+
+        drawerToggle = object : ActionBarDrawerToggle(
+            this@TimerActivity,
+            drawer,
+            toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        ) {
+            override fun onDrawerOpened(drawerView: View) {
+                super.onDrawerOpened(drawerView)
+                actionBar?.title = title
+            }
+
+            override fun onDrawerClosed(drawerView: View) {
+                super.onDrawerClosed(drawerView)
+                actionBar?.title = drawer.getDrawerTitle(Gravity.LEFT)
+            }
+        }
+
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        navigation.setNavigationItemSelectedListener {
+            return@setNavigationItemSelectedListener when (it.itemId) {
+                R.id.menu_privacy -> {
+                    val intent = Intent(
+                        Intent.ACTION_VIEW,
+                        // TODO : Remote Config から取得するようにする
+                        Uri.parse(
+                            "https://pagehosting-d362c.firebaseapp.com/" +
+                                "proctimer/privacy-poricy.html"
+                        )
+                    )
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, TimerFragment.newInstance())
+                .replace(R.id.frame, TimerFragment.newInstance())
                 .commitNow()
+        }
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        drawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        drawerToggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(navigation)) {
+            drawer.closeDrawer(navigation)
+        } else {
+            super.onBackPressed()
         }
     }
 
