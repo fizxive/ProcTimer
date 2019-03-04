cf4da5389cd86747589925af80a87c2854a3630e
fizxive <imaginary.rhombus@gmail.com>
2019-03-04 20:07:29 +0900

    ViewModel テスト時の取得方法を koin による DI に委ねる.

--- app/src/test/java/com/imaginaryrhombus/proctimer/TimerViewModelTest.kt
+++ app/src/test/java/com/imaginaryrhombus/proctimer/TimerViewModelTest.kt
@@ -8,6 +8,7 @@ import org.junit.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.standalone.get
 import org.koin.test.AutoCloseKoinTest
 
 @RunWith(AndroidJUnit4::class)
@@ -25,24 +26,22 @@ class TimerViewModelTest : AutoCloseKoinTest() {
      */
     @Test
     fun testText() {
-        scenarioRule.scenario.onActivity {
-            val timerViewModel = TimerViewModel(it.application)
-            val method =
-                timerViewModel.javaClass.getMethod(
-                    "createTimerStringFromSeconds", Float::class.java
-                )
+        val timerViewModel = get<TimerViewModel>()
+        val method =
+            timerViewModel.javaClass.getMethod(
+                "createTimerStringFromSeconds", Float::class.java
+            )
 
-            assertNotNull(method)
-            method.isAccessible = true
+        assertNotNull(method)
+        method.isAccessible = true
 
-            assertEquals("00:30", method.invoke(null, 30.0f))
-            assertEquals("01:00", method.invoke(null, 60.0f))
-            assertEquals("01:15", method.invoke(null, 75.0f))
-            assertEquals("00:00", method.invoke(null, 0.0f))
-            assertEquals("00:00", method.invoke(null, -10.0f))
-            assertEquals("00:11", method.invoke(null, 10.110f))
-            assertEquals("01:01", method.invoke(null, 60.085f))
-            assertEquals("01:07", method.invoke(null, 66.666f))
-        }
+        assertEquals("00:30", method.invoke(null, 30.0f))
+        assertEquals("01:00", method.invoke(null, 60.0f))
+        assertEquals("01:15", method.invoke(null, 75.0f))
+        assertEquals("00:00", method.invoke(null, 0.0f))
+        assertEquals("00:00", method.invoke(null, -10.0f))
+        assertEquals("00:11", method.invoke(null, 10.110f))
+        assertEquals("01:01", method.invoke(null, 60.085f))
+        assertEquals("01:07", method.invoke(null, 66.666f))
     }
 }
