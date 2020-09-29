efb5d9976c10e6f1c7f17817a7c511efba40899c
FIzxive <imaginary.rhombus@gmail.com>
2020-09-29 21:25:54 +0900

    実行時エラーの解消
    Koinで明示的にクラスを指定していなかったために実行時エラーになっていたのを修正

--- app/src/main/java/com/imaginaryrhombus/proctimer/TimerApplication.kt
+++ app/src/main/java/com/imaginaryrhombus/proctimer/TimerApplication.kt
@@ -4,9 +4,11 @@ import android.app.Application
 import com.imaginaryrhombus.proctimer.application.TimerComponent
 import com.imaginaryrhombus.proctimer.application.TimerComponentInterface
 import com.imaginaryrhombus.proctimer.application.TimerRemoteConfigClient
+import com.imaginaryrhombus.proctimer.application.TimerRemoteConfigClientInterface
 import com.imaginaryrhombus.proctimer.application.TimerSharedPreferencesComponent
 import com.imaginaryrhombus.proctimer.ui.timer.TimerViewModel
 import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -19,6 +21,7 @@ class TimerApplication : Application() {
         super.onCreate()
         startKoin {
             androidLogger(Level.NONE)
+            androidContext(this@TimerApplication)
             modules(timerComponentModule, timerViewModelModule)
         }
     }
@@ -26,7 +29,7 @@ class TimerApplication : Application() {
     private val timerComponentModule = module {
         single<TimerComponentInterface> { TimerComponent(androidApplication()) }
         single { TimerSharedPreferencesComponent(get()) }
-        single { TimerRemoteConfigClient() }
+        single<TimerRemoteConfigClientInterface> { TimerRemoteConfigClient() }
     }
 
     private val timerViewModelModule = module {
